5a6e6e88a4d596de0c720ab2eedb3c9333cbab95
Jonnathan Bucheli Galindo <joobga@gmail.com>
2018-05-25 16:54:00 -0500

    Add files via upload

---
+++ CG/index.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Theme Made By www.w3schools.com - No Copyright -->
+  <title>COMPUTACIÓN GRÁFICA</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+  body {
+      font: 20px Montserrat, sans-serif;
+      line-height: 1.8;
+      color: #f5f6f7;
+  }
+  p {font-size: 16px;}
+  .margin {margin-bottom: 45px;}
+  .bg-1 { 
+      background-color: #1abc9c; /* Green */
+      color: #ffffff;
+  }
+  .bg-2 { 
+      background-color: #474e5d; /* Dark Blue */
+      color: #ffffff;
+  }
+  .bg-3 { 
+      background-color: #ffffff; /* White */
+      color: #555555;
+  }
+  .bg-4 { 
+      background-color: #2f2f2f; /* Black Gray */
+      color: #fff;
+  }
+  .container-fluid {
+      padding-top: 70px;
+      padding-bottom: 70px;
+  }
+  .navbar {
+      padding-top: 15px;
+      padding-bottom: 15px;
+      border: 0;
+      border-radius: 0;
+      margin-bottom: 0;
+      font-size: 12px;
+      letter-spacing: 5px;
+  }
+  .navbar-nav  li a:hover {
+      color: #1abc9c !important;
+  }
+  </style>
+</head>
+<body>
+
+<!-- Navbar -->
+<nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">CG</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#intro">Introducción</a></li>
+        <li><a href="#equipo">Integrantes</a></li>
+        <li><a href="#actividades">Actividades</a></li>
+       
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- First Container -->
+<div class="container-fluid bg-1 text-center">
+  <h1 class="margin">COMPUTACION GRAFICA</h1> 
+  <h3>Universidad Nacional Abierta y a Distancia</h3>
+</div>
+
+<!-- Second Container -->
+<div id="intro" class="container-fluid bg-2 text-center">
+  <h3 class="margin">Introducción</h3>
+  <p>La presentación del portafolio de servicios se ha demostrado a la empresa SIRYS la innovación y habilidades en el desarrollo del Diseño Computacional, donde se presentó el logo e imagen corporativa que la empresa tendrá y sobresaldrá en el mercado, el conocimiento en la creación de Figuras en 2D y escenarios virtuales en 3D los cuales dará ese toque interactivo y de curiosidad a los clientes que tengan el privilegio de visualizar la presentación brindada por la empresa SIRYS y sabrán que es la mejor opción para su empresa.
+</p>
+<h3 class="margin">Descripción Proyecto</h3>
+<p>
+  La empresa SIRYS necesita que su presentación sea dinámica, animada, con figuras que sufran transformaciones, cambien de color, posición de acuerdo a las acciones del mouse o teclado, nosotros como grupo de trabajo de la empresa Marketing Digital SAS, sabemos que somos la mejor opción para su necesidad por lo cual presentaremos un nn esquema minimalista, donde primera pantalla será una imagen total o un video. Impactando y atrapando al usuario, cada icono y animación será similar y adecuada al elemento a representar representando estandarización, los acceso a cada producto podrán ser activados por mouse o teclado, los texto de información serán sutiles, no extensos, si se sea entregar más información será a través de audio o preferiblemente video. Y su tipografía especialmente sin serifas. Contraste entre diapositivas o pantallas usan el mismo color, tipo y tamaño de letra, diferenciando el término destacar elementos además de repetir el estilo de la página en toda la presentación creando consistencia. los títulos del mismo color, alineación de los textos, imágenes y elementos.
+
+<p>
+</div>
+
+<!-- Third Container (Grid) -->
+<div id="equipo" class="container-fluid bg-4 text-center">    
+  <h3 class="margin">Equipo de Trabajo</h3><br>
+  <div class="row">
+    <div class="col-sm-3">
+      <p>JAVIER JARAMILLO PATIÑO</p>
+      <img src="birds1.jpg" class="img-responsive margin" style="width:100%" alt="Image">
+      <p>Estudiante de Ingeniería de sistemas de la Universidad Nacional Abierta y a Distancia, estudiante de último semestre.
+
+Trabaja en el día y estudia en las noches, una persona muy dedicada y centrada en lo que quiere, le gusta soñar en grande y quiere tener su propia empresa de desarrollo de software.
+</p>
+    </div>
+    <div class="col-sm-3"> 
+      <p>JHON STALING SEVILLANO ZEMANATE</p>
+      <img src="birds2.jpg" class="img-responsive margin" style="width:100%" alt="Image">
+      <p>Estudiante de Ingeniería de sistemas de la Universidad Nacional Abierta y a Distancia UNAD en proceso de Homologación. 
+Soy tecnólogo en Desarrollo de Software, y actualmente me encuentro Trabajando como Analista de Calidad Semi-senior en Seratic LTDA, empresa de Desarrollo de Software, con una experiencia de 6 años. 
+Quiero terminar mi carrera de Ingeniería de Sistemas, para poder realizar la especialización de Seguridad Informática.
+</p>
+    </div>
+    <div class="col-sm-3"> 
+      <p>MARLODY GOMEZ URBANO</p>
+      <img src="birds3.jpg" class="img-responsive margin" style="width:100%" alt="Image">
+      <p>Estudiante de Ingeniería de Sistemas la Universidad Nacional Abierta y A Distancia, actualmente estudiante del programa de Seguridad Informática, trabajo en la Secretaria de Educación Departamental de Nariño.</p>
+    </div>
+    <div class="col-sm-3"> 
+      <p>JONNATHAN BUCHELI GALINDO</p>
+      <img src="birds3.jpg" class="img-responsive margin" style="width:100%" alt="Image">
+      <p>Desarrollador WEB de herramientas colaborativas y de Gobierno Abierto, Colaborador de Inteligencia Colectiva en MediaLAB Prado Madrid y Finalista 6k de Iniciativas de Herramientas de Anticorrupción de Colombia Compra Eficiente. Entre los principales desarrollos están BuscoHechos hace parte de ciudadesabiertas.mx, Declaraciones Juramentadas, GANAcontrol la cual esta hacen parte de softwarepublicocolombia.gov.co y ganadora de los premios Indigo 2017.</p>
+    </div>
+  </div>
+</div>
+
+<div class="container-fluid bg-3 " id="actividades">    
+  <h3 class="margin text-center">ACTIVIDADES REALIZADAS</h3><br>
+  <div class="row" >
+    <div class="col-sm-4">
+      <h3>Conocer el planteamiento del problema del proyecto a desarrollar</h3>
+    </div> 
+    <div class="col-sm-8">
+      <p>Teniendo en cuenta el trabajo de equipo el cual desarrollar la estrategia, se reconoció el grupo de trabajo y se dio conocer o crear las herramientas tecnológicas para contacto y comunicación y se analizó e identificó el problema a entregar.
+Producto entregado: Plantilla en formato solicitado con las especificaciones de la temática, datos personales, y análisis del problema.
+      </p>
+    </div> 
+  </div>
+  <div class="row" style="margin-top:5%;">    
+    <div class="col-sm-8">
+      <p>Con el fin de crear el producto final, en esta etapa se instaló el software necesario para su desarrollo en el cual se modificó y creó figuras primitivas que darían el conocimiento previo de la herramienta y el lenguaje de programación.
+Producto entregado: Manual de instalación de herramienta. y Guía de creación de figuras primitivas.</p>
+    </div> 
+    <div class="col-sm-4">
+      <h3>Analizar la información</h3>
+    </div> 
+  </div>
+  <div class="row" style="margin-top:5%;">
+    <div class="col-sm-4">
+      <h3>Brindar solución al problema</h3>
+    </div> 
+    <div class="col-sm-8">      
+      <p>Con el fin de crear el producto final, en esta etapa se instaló el software necesario para su desarrollo en el cual se modificó y creó figuras primitivas que darían el conocimiento previo de la herramienta y el lenguaje de programación.
+Producto entregado: Manual de instalación de herramienta. y Guía de creación de figuras primitivas.</p>
+    </div> 
+  </div> 
+      <style>
+    h2 {
+      background-color: #004568;
+      box-shadow: 2px 2px 10px #666;
+      padding: 0.25em;
+      color:#fff;
+    }
+
+    div.espacio3D {
+      width: 300px;
+      height: 300px;
+      margin: 3em auto 0 auto;
+      border: 1px solid rgba(0,255,0,0.5);
+      position: relative;
+      perspective-origin: center -50%;
+      perspective: 500px;
+      transform: scale(0.75);
+    }
+
+    div.cubo3D {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      transform-style: preserve-3d;
+      transform: translateZ(-150px);
+      animation-name: gira;
+      animation-duration: 3000ms;
+      animation-iteration-count: infinite;
+    }
+
+    div.base {
+      position: absolute;
+      width: 150px;
+      height: 150px;
+      background-color: rgba(0,0,0,0.15);
+      transform: translateX(75px) translateY(75px) rotateX(90deg) translateZ(-200px);
+      box-shadow: 0 0 50px 100px rgba(0,0,0,0.15);
+    }
+    aside.cara {
+      position: absolute;
+      width: 298px;
+      height: 298px;
+      background-color: rgba(232,178,0,0.25);
+      border: 1px solid rgba(255,150,50,0.75);
+      color: #004568;
+      font-size: 2em;
+      text-align: center;
+      line-height: 298px;
+      box-shadow: 0px 0px 5px rgba(255,150,50,0.75);
+    }
+
+    aside.cara1 { /* Frente */
+      transform: translateZ(150px);
+    }
+
+    aside.cara2 { /* Atrás */
+      transform: rotateY(180deg) translateZ(150px);
+    }
+
+    aside.cara3 { /* Izquierda */
+      transform: rotateY(-90deg) translateZ(150px);
+    }
+
+    aside.cara4 { /* derecha */
+      transform: rotateY(90deg) translateZ(150px);
+    }
+
+    aside.cara5 { /* abajo */
+      transform: rotateX(-90deg) translateZ(150px);
+    }
+
+    aside.cara6 { /* arriba */
+      transform: rotateX(90deg) translateZ(150px);
+    }
+
+    @keyframes gira {
+      0% {
+        transform: translateZ(-150px) rotateY(0deg);
+      }
+      100% {
+        transform: translateZ(-150px) rotateY(360deg);
+      }
+    }
+    </style>
+
+      <div class="espacio3D">
+        <div class="cubo3D">
+          <div class="base"></div>
+          <aside class="cara cara1">Conputación</aside>
+          <aside class="cara cara2">Gráfica</aside>
+          <aside class="cara cara3">Grupo 13</aside>
+          <aside class="cara cara4">2018</aside>
+          <aside class="cara cara5">UNAD</aside>
+          <aside class="cara cara6"></aside>
+        </div><!-- termina cubo 3d -->
+      </div><!-- termina espacio 3d -->   
+</div> 
+
+
+<!-- Footer -->
+<footer class="container-fluid bg-4 ">
+<h3 class="margin">Concluciones</h3>
+<li>La identificación y solución de problemas generales de una empresa se pueden adecuar elementos gráficos e interactivos los cuales dan un valor agregado e impactante a la presentación de productos.</li>
+<li>El conocimiento de herramientas o lenguajes bases son indispensables en las nuevas tecnologías y proyectos.</li>
+<li>La entrega de un producto final debe realizarse paso a paso analizando e identificando el problema a resolver y acompañado de un buen grupo de trabajo.</li>
+<br/>
+
+  <p>Grupo 13 - Computación Gráfica</p> 
+  <p>TUTOR HERNANDO JOSE PEÑARANDA</p>
+</footer>
+
+</body>
+</html>
